e4d1df5b2a0e2915522ebd50d9dbcce88b225d8c
Gioele Giusti <gioele.pg@gmail.com>
2024-09-11 20:50:21 +0200

    Add netcode plugin

--- src/Paper3Networking/Assets/Scripts/UIManager.cs
+++ src/Paper3Networking/Assets/Scripts/UIManager.cs
@@ -51,10 +51,11 @@ public class UIManager : Singleton<UIManager>
         startHostButton?.onClick.AddListener(async () =>
         {
             // this allows the UnityMultiplayer and UnityMultiplayerRelay scene to work with and without
-            // relay features - if the Unity transport is found and is relay protocol then we redirect all the 
+            // relay features - if the Unity transport is found and is relay protocol then we redirect all the
             // traffic through the relay, else it just uses a LAN type (UNET) communication.
-            if (RelayManager.Instance.IsRelayEnabled) 
-                await RelayManager.Instance.SetupRelay();
+
+            // if (RelayManager.Instance.IsRelayEnabled)
+            //     await RelayManager.Instance.SetupRelay();
 
             if (NetworkManager.Singleton.StartHost())
                 Logger.Instance.LogInfo("Host started...");
@@ -65,8 +66,8 @@ public class UIManager : Singleton<UIManager>
         // START CLIENT
         startClientButton?.onClick.AddListener(async () =>
         {
-            if (RelayManager.Instance.IsRelayEnabled && !string.IsNullOrEmpty(joinCodeInput.text))
-                await RelayManager.Instance.JoinRelay(joinCodeInput.text);
+            // if (RelayManager.Instance.IsRelayEnabled && !string.IsNullOrEmpty(joinCodeInput.text))
+            //     await RelayManager.Instance.JoinRelay(joinCodeInput.text);
 
             if(NetworkManager.Singleton.StartClient())
                 Logger.Instance.LogInfo("Client started...");
@@ -85,7 +86,7 @@ public class UIManager : Singleton<UIManager>
             hasServerStarted = true;
         };
 
-        executePhysicsButton.onClick.AddListener(() => 
+        executePhysicsButton.onClick.AddListener(() =>
         {
             if (!hasServerStarted)
             {
